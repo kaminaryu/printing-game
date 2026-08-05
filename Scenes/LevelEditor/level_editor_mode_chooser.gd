6713927d2985b5c23790fb939781c327edb2d593
IzzAmani <izzryugasaki@gmail.com>
2026-08-05 19:11:49 +0800

    Feat: Level Editor mode chooser

---
+++ Scenes/LevelEditor/level_editor_mode_chooser.gd
@@ -0,0 +1,8 @@
+extends PanelContainer
+
+
+func _on_from_scratch_button_up() -> void:
+	get_tree().change_scene_to_file("res://Scenes/LevelEditor/level_editor_paper_setup.tscn")
+
+func _on_load_level_button_up() -> void:
+	get_tree().change_scene_to_file("res://Scenes/LevelEditor/level_editor_level_loader.tscn")
