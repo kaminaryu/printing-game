d0fa7bd25547c4c770d57c390f47a4654abd789c
IzzAmani <izzryugasaki@gmail.com>
2026-08-16 20:58:04 +0800

    Feat: Revert back to static paintbrush color
    Meh: Also added mobile port for fun hwuheuwheuhewuehuw

--- Objects/Printer/PrintingGrid/painter.gd
+++ Objects/Printer/PrintingGrid/painter.gd
@@ -12,8 +12,6 @@ func _ready() -> void:
 	$Button.button_down.connect(_on_button_down)
 	$Button.mouse_entered.connect(_on_mouse_entered)
 	$Button.mouse_exited.connect(_on_mouse_exited)
-	ColorManager.color_changed.connect(_change_painter_color)
-	$BrushItself.modulate = Color.from_string(ColorManager.get_selected_color(), "#000")
 
 
 func show_col_shadow() -> void :
@@ -23,11 +21,6 @@ func show_col_shadow() -> void :
 func show_row_shadow() -> void :
 	$BrushItself/ShadowRow.show()
 
-
-func _change_painter_color() -> void :
-	var color_key := ColorManager.get_selected_color()
-
-	$BrushItself.modulate = Color.from_string(color_key, "#000")
 
 
 func _on_button_down() -> void :
@@ -48,7 +41,3 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	CursorManager.set_cursor()
 	unhovered.emit()
-
-
-func _on_ink_channel_change() -> void :
-	_change_painter_color()
